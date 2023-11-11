a51aca3f1623ff455babc7680ab3f02ed1adfdcf
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-11-11 14:08:43 -0500

    Finish credits and connect scenes

---
+++ Magnuson_Brian_Project_CMG/Assets/Scripts/FlipCredits.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCredits : MonoBehaviour
+{
+    [SerializeField]
+    private float initialDelay = 5f;
+
+    [SerializeField]
+    private float timeDelay = 5f;
+
+    [SerializeField]
+    private GameObject displayAfterCredits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RunCredits());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator RunCredits()
+    {
+        yield return new WaitForSeconds(initialDelay);
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(timeDelay);
+            child.gameObject.SetActive(false);
+        }
+        if (displayAfterCredits != null)
+            displayAfterCredits.SetActive(true);
+    }
+}
